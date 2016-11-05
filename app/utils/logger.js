bd7a8d0b62cd96d3c83a87b0bbb7677ea896d540
kfs <630353983@qq.com>
2016-11-05 18:11:15 +0800

    日志管理 - 问题修复

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,24 +1,28 @@
 /**
  * User: kfs
  * Date：2016/11/5
- * Desc：日志
+ * Desc：日志管理
  */
 var log4js = require('log4js');
 var env = process.env.NODE_ENV || 'development';
+var config = require('../config/config')[env];
 
 log4js.configure({
     appenders: [
         { type: 'console' }, //控制台输出
         {
-            type: 'file', //文件输出
+            type: 'DateFile', //文件输出
             filename: '../logs/access.log',
+            pattern: "_yyyy-MM-dd.log",
+            alwaysIncludePattern: true,
             maxLogSize: 1024,
             backups:3,
-            category: 'normal'
-        }
-    ]
+            category: 'log'
+        },
+    ],
+    replaceConsole: true
 });
-var logger = log4js.getLogger('cheese');
+var logger = log4js.getLogger('log');
 logger.setLevel(env == 'development' ? 'DEBUG' : 'ERROR');
 
 module.exports = logger;
